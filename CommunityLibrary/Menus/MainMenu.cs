a14bbfcbd71e6d105ea671880fdfadd954c456ad
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2022-05-15 18:27:59 +1000

    Refactored

--- CommunityLibrary/Menus/MainMenu.cs
+++ CommunityLibrary/Menus/MainMenu.cs
@@ -1,39 +1,10 @@
 ﻿using System;
+
 namespace CommunityLibrary
 {
 	public class MainMenu
 	{
-		public static void DisplayMainMenu()
-		{
-			Options();
-
-			string choice = "";
-
-			while (!choice.Equals("1") || !choice.Equals("2") || !choice.Equals("0"))
-			{
-				choice = Console.ReadLine();
-
-				if (choice.Equals("1"))
-				{
-					StaffMenu.DisplayStaffLogin();
-					Options(); // when user exits staff menu, display main menu again
-				}
-				else if (choice.Equals("2"))
-				{
-					MemberMenu.DisplayMemberLogin();
-					Options(); // when user exits member menu, display main menu again
-				}
-				else if (choice.Equals("0"))
-				{
-					Console.Clear();
-					Console.WriteLine("Exiting program...");
-					return; // return to MAIN
-				}
-				else Console.Write("Invalid choice, please try again: ");
-			}
-		}
-
-		private static void Options()
+		private static void Header()
         {
 			Console.Clear();
 			Console.Write("===================================================================\n" +
@@ -41,12 +12,38 @@ namespace CommunityLibrary
 				"█▀▀ █▀█ █▀▄▀█ █▀▄▀█ █░█ █▄░█ █ ▀█▀ █▄█	█░░ █ █▄▄ █▀█ ▄▀█ █▀█ █▄█\n" +
 				"█▄▄ █▄█ █░▀░█ █░▀░█ █▄█ █░▀█ █ ░█░ ░█░	█▄▄ █ █▄█ █▀▄ █▀█ █▀▄ ░█░\n" +
 				"\n=============================Main Menu=============================\n\n");
+		}
 
+		private static void Options()
+		{
 			Console.WriteLine("1. Staff Login");
 			Console.WriteLine("2. Member Login");
 			Console.WriteLine("0. Exit");
 
 			Console.Write("\nEnter your choice ==> (1/2/0): ");
+		}
+
+		public static void DisplayMainMenu()
+		{
+			Header();
+			Options();
+
+			string choice = "";
+
+			while (!choice.Equals("0"))
+			{
+				choice = Console.ReadLine();
+
+				if (choice.Equals("1")) StaffMenu.DisplayStaffLogin();
+				else if (choice.Equals("2")) MemberMenu.DisplayMemberLogin();
+
+				// when user completes action, display options again
+				Header();
+				Options();
+
+				// additional line if user input was invalid
+				if (!choice.Equals("1") && !choice.Equals("2") && !choice.Equals("0")) Console.Write("Invalid choice, please try again: ");
+			}
 		}
 	}
 }
--- CommunityLibrary/Menus/StaffMenu.cs
+++ CommunityLibrary/Menus/StaffMenu.cs
@@ -1,16 +1,36 @@
 ﻿using System;
+
 namespace CommunityLibrary
 {
 	public class StaffMenu
 	{
-		public static void DisplayStaffLogin()
+		private static void Header()
 		{
 			Console.Clear();
 			Console.Write("===================================================================\n" +
 				"Johnny n Jamie's\n" +
 				"█▀▀ █▀█ █▀▄▀█ █▀▄▀█ █░█ █▄░█ █ ▀█▀ █▄█	█░░ █ █▄▄ █▀█ ▄▀█ █▀█ █▄█\n" +
 				"█▄▄ █▄█ █░▀░█ █░▀░█ █▄█ █░▀█ █ ░█░ ░█░	█▄▄ █ █▄█ █▀▄ █▀█ █▀▄ ░█░\n" +
-				"\n=============================Staff Menu============================\n");
+				"\n=============================Staff Menu============================\n\n");
+		}
+
+		private static void Options()
+		{
+			Console.WriteLine("1. Add new DVDs of a new movie to the system");
+			Console.WriteLine("2. Remove DVDs of a movie from the system");
+			Console.WriteLine("3. Register a new member with the system");
+			Console.WriteLine("4. Remove a registered member from the system");
+			Console.WriteLine("5. Display a member's contact phone number, given the member's name");
+			Console.WriteLine("6. Display all members who are currently renting a particular movie");
+			Console.WriteLine("0. Return to the main menu");
+
+			Console.Write("\nEnter your choice ==> (1/2/3/4/5/6/0): ");
+		}
+
+		public static void DisplayStaffLogin()
+		{
+			Header();
+			Console.WriteLine("Please login with a staff account");
 
 			bool auth = false;
 			string username;
@@ -27,8 +47,7 @@ namespace CommunityLibrary
 				else
 				{
 					Console.WriteLine("\nInvalid credentials...\n" +
-					"Enter any key to try again\n" +
-					"Enter 0 to return to main menu\n");
+					"Enter any key to try again, enter 0 to return to main menu\n");
 
 					if (Console.ReadLine().Equals("0")) return; // return to MAINMENU
 				}
@@ -37,34 +56,57 @@ namespace CommunityLibrary
 			DisplayStaffMenu();
 		}
 
-		public static void DisplayStaffMenu()
+		private static void DisplayStaffMenu()
         {
-			Console.Clear();
-			Console.Write("===================================================================\n" +
-				"Johnny n Jamie's\n" +
-				"█▀▀ █▀█ █▀▄▀█ █▀▄▀█ █░█ █▄░█ █ ▀█▀ █▄█	█░░ █ █▄▄ █▀█ ▄▀█ █▀█ █▄█\n" +
-				"█▄▄ █▄█ █░▀░█ █░▀░█ █▄█ █░▀█ █ ░█░ ░█░	█▄▄ █ █▄█ █▀▄ █▀█ █▀▄ ░█░\n" +
-				"\n=============================Staff Menu============================\n\n");
-
-			Console.WriteLine("1. Add new DVDs of a new movie to the system");
-			Console.WriteLine("2. Remove DVDs of a movie from the system");
-			Console.WriteLine("3. Register a new member with the system");
-			Console.WriteLine("4. Remove a registered member from the system");
-			Console.WriteLine("5. Display a member's contact phone number, given the member's name");
-			Console.WriteLine("6. Display all members who are currently renting a particular movie");
-			Console.WriteLine("0. Return to the main menu");
-
-			Console.Write("\nEnter your choice ==> (1/2/3/4/5/6/0): ");
+			Header();
+			Options();
 
 			string choice = "";
 
-			while (!choice.Equals("1") || !choice.Equals("2") || !choice.Equals("3") || !choice.Equals("4") ||
-				!choice.Equals("5") || !choice.Equals("6") || !choice.Equals("0"))
+			// loop so the user can select options...
+            // if user selects to exit, break out of loop and return to end of DISPLAYSTAFFLOGIN which then returns to MAINMENU
+			while (!choice.Equals("0"))
 			{
 				choice = Console.ReadLine();
 
-				if (choice.Equals("0")) return; // return to end of DISPLAYSTAFFLOGIN which then returns to MAINMENU
-				else Console.Write("Invalid choice, please try again: ");
+				if (choice.Equals("1")) /* todo */;
+				else if (choice.Equals("2")) /* todo */;
+				else if (choice.Equals("3")) RegisterMember();
+				else if (choice.Equals("4")) /* todo */;
+				else if (choice.Equals("5")) /* todo */;
+				else if (choice.Equals("6")) /* todo */;
+
+				// when user completes action, display options again
+				Header();
+				Options();
+
+				// additional line if user input was invalid
+				if (!choice.Equals("1") && !choice.Equals("2") && !choice.Equals("3") && !choice.Equals("4") &&
+				!choice.Equals("5") && !choice.Equals("6") && !choice.Equals("0")) Console.Write("Invalid choice, please try again: ");
+			}
+		}
+
+		// OPTION 3
+		private static void RegisterMember()
+        {
+			Header();
+
+			Console.Write("First name: ");
+			string first = Console.ReadLine();
+			Console.Write("Last name: ");
+			string last = Console.ReadLine();
+			Console.Write("Phone number: ");
+			string phone = Console.ReadLine();
+			Console.Write("Pin: ");
+			string pin = Console.ReadLine();
+
+			Console.Write("Enter any key to register user, 0 to cancel: ");
+
+			if (!Console.ReadLine().Equals("0"))
+			{
+				IMember m = new Member(first, last, phone, pin);
+				IMovieCollection borrowing = new MovieCollection();
+				Records.reg.Add(m, borrowing);
 			}
 		}
 	}
