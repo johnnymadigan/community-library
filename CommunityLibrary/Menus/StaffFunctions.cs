1733e22d91691fab7e101afd8196233810c857b0
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2022-05-16 16:48:36 +1000

    staff can now add/remove DVDs
    added 2 new functionalities

--- CommunityLibrary/Menus/StaffFunctions.cs
+++ CommunityLibrary/Menus/StaffFunctions.cs
@@ -3,24 +3,39 @@ namespace CommunityLibrary
 {
 	public class StaffFunctions
 	{
-		// OPTION 3 FUNCTIONALITY (seperate + public for testing)
-		public static bool RegisterMember(IMember m, IMovieCollection borrowing)
+		// OPTION 1 FUNCTIONALITY (seperate + public for testing)
+		public static bool AddDVD(string t, MovieGenre g, MovieClassification c, int d, int n)
 		{
-			if (!IMember.IsValidContactNumber(m.ContactNumber)) return false;
-			else if (!IMember.IsValidPin(m.Pin)) return false;
+			IMovie m = new Movie(t, g, c, d, n);
+			return Records.lib.Insert(m); // true if inserted into BST, false if dupe
+		}
+
+		// OPTION 2 FUNCTIONALITY (seperate + public for testing)
+		public static bool RemoveDVD(string t)
+		{
+			IMovie m = new Movie(t);
+			return Records.lib.Delete(m); // true if removed from BST, false if not found
+		}
+
+		// OPTION 3 FUNCTIONALITY (seperate + public for testing)
+		public static bool RegisterMember(string first, string last, string phone, string pin)
+		{
+			if (!IMember.IsValidContactNumber(phone) || !IMember.IsValidPin(pin)) return false;
 			else
 			{
+				IMember m = new Member(first, last, phone, pin);
+				IMovieCollection borrowing = new MovieCollection();
 				Records.reg.Add(m, borrowing);
 				return true;
 			}
 		}
 
 		// OPTION 4 FUNCTIONALITY (seperate + public for testing)
-		public static bool DeregisterMember(IMember m)
+		public static bool DeregisterMember(string first, string last)
 		{
 			foreach (IMember k in Records.reg.Keys)
 			{
-				if (k.FirstName == m.FirstName && k.LastName == m.LastName)
+				if (k.FirstName == first && k.LastName == last)
 				{
 					Records.reg.Remove(k);
 					return true;
--- CommunityLibrary/Menus/StaffMenu.cs
+++ CommunityLibrary/Menus/StaffMenu.cs
@@ -58,8 +58,8 @@ namespace CommunityLibrary
 			{
 				string choice = Console.ReadLine();
 
-				if (choice.Equals("1")) /* todo */;
-				else if (choice.Equals("2")) /* todo */;
+				if (choice.Equals("1")) AddDVD();
+				else if (choice.Equals("2")) RemoveDVD();
 				else if (choice.Equals("3")) RegisterMember();
 				else if (choice.Equals("4")) DeregisterMember();
 				else if (choice.Equals("5")) /* todo */;
@@ -74,13 +74,144 @@ namespace CommunityLibrary
 			}
 		}
 
+		// OPTION 1 USER INPUT/LOOP
+		// ??
+		// Pre-condition: nil
+		// Post-condition: nil
+		private static void AddDVD()
+		{
+			while (true)
+			{
+				Header();
+				Console.WriteLine("Please enter info for movie to add...");
+
+				// it is assumed full names are unique
+				// todo might check if name is not taken tho
+				Console.Write("Movie title: ");
+				string t = Console.ReadLine();
+
+				string genreInput;
+				MovieGenre? g = null;
+				        
+				while (g == null)
+				{
+					Console.Write("\n1. Action\n2. Comedy\n3. Drama\n4. History\n5. Western\nSelect genre ==> (1/2/3/4/5): ");
+					genreInput = Console.ReadLine();
+
+					if (genreInput.Equals("1")) g = MovieGenre.Action;
+					else if (genreInput.Equals("2")) g = MovieGenre.Comedy;
+					else if (genreInput.Equals("3")) g = MovieGenre.Drama;
+					else if (genreInput.Equals("4")) g = MovieGenre.History;
+					else if (genreInput.Equals("5")) g = MovieGenre.Western;
+					else
+					{
+						Console.Write("\nInvalid genre...\nEnter any key to try again, 0 to cancel: ");
+						if (Console.ReadLine().Equals("0")) return;
+					}
+				}
+
+				string classInput;
+				MovieClassification? c = null;
+				while (c == null)
+				{
+					Console.Write("\n1. G\n2. PG\n3. M\n4. M15Plus\nSelect classification ==> (1/2/3/4): ");
+					classInput = Console.ReadLine();
+					
+					if (classInput.Equals("1")) c = MovieClassification.G;
+					else if (classInput.Equals("2")) c = MovieClassification.PG;
+					else if (classInput.Equals("3")) c = MovieClassification.M;
+					else if (classInput.Equals("4")) c = MovieClassification.M15Plus;
+					else
+					{
+						Console.Write("\nInvalid classification...\nEnter any key to try again, 0 to cancel: ");
+						if (Console.ReadLine().Equals("0")) return;
+					}
+				}
+
+				string dInput;
+				int d;
+				while (true)
+                {
+					Console.Write("Duration: ");
+					dInput = Console.ReadLine();
+
+					if (int.TryParse(dInput, out d)) break;
+					else
+					{
+						Console.Write("\nDuration must be a number...\nEnter any key to try again, 0 to cancel: ");
+						if (Console.ReadLine().Equals("0")) return;
+					}
+				}
+
+				string nInput;
+				int n;
+				while (true)
+				{
+					Console.Write("Total copies: ");
+					nInput = Console.ReadLine();
+
+					if (int.TryParse(nInput, out n)) break;
+					else
+					{
+						Console.Write("\nTotal copies must be a number...\nEnter any key to try again, 0 to cancel: ");
+						if (Console.ReadLine().Equals("0")) return;
+					}
+				}
+
+				Console.WriteLine($"\nNew movie: {t}");
+				Console.Write("Enter any key to add this movie, 0 to cancel: ");
+
+				if (Console.ReadLine().Equals("0")) return;
+
+				if (StaffFunctions.AddDVD(t, (MovieGenre)g, (MovieClassification)c, d, n))
+				{
+					Console.Write($"Movie {t} added...\nEnter any key to continue: ");
+					Console.ReadLine();
+					return;
+				}
+				else
+				{
+					Console.Write($"\nMovie {t} is a duplicate...\nEnter any key to try again, 0 to cancel: ");
+					if (Console.ReadLine().Equals("0")) return;
+				}
+			}
+		}
+
+		// OPTION 2 USER INPUT/LOOP
+		// ??
+		// Pre-condition: nil
+		// Post-condition: nil
+		private static void RemoveDVD()
+		{
+			while (true)
+			{
+				Header();
+				Console.WriteLine("Please enter info for movie to delete...");
+
+				Console.Write("Movie title: ");
+				string t = Console.ReadLine();
+
+				if (StaffFunctions.RemoveDVD(t))
+				{
+					Console.Write($"\nMovie {t} removed...\nEnter any key to continue: ");
+					Console.ReadLine();
+					return;
+				}
+				else
+				{
+					Console.Write($"\nMovie {t} does not exist...\nEnter any key to try again, 0 to cancel: ");
+					if (Console.ReadLine().Equals("0")) return;
+				}
+			}
+		}
+
 		// OPTION 3 USER INPUT/LOOP
 		private static void RegisterMember()
         {
 			while (true)
 			{
 				Header();
-				Console.WriteLine("Please enter new member information...");
+				Console.WriteLine("Please enter info for new member...");
 
 				// it is assumed full names are unique
 				// todo might check if name is not taken tho
@@ -98,30 +229,30 @@ namespace CommunityLibrary
 
 				if (Console.ReadLine().Equals("0")) return;
 
-				IMember m = new Member(first, last, phone, pin);
-				IMovieCollection borrowing = new MovieCollection();
-
-				if (StaffFunctions.RegisterMember(m, borrowing))
+				if (StaffFunctions.RegisterMember(first, last, phone, pin))
 				{
-					Console.Write("\nMember added...\nEnter any key to continue: ");
+					Console.Write($"\nMember {first} {last} added...\nEnter any key to continue: ");
 					Console.ReadLine();
 					return;
 				}
 				else
 				{
-					Console.Write("\nInvalid phone or pin...\nEnter any key to try again, 0 to cancel: ");
+					Console.Write("\nInvalid phone/pin...\nEnter any key to try again, 0 to cancel: ");
 					if (Console.ReadLine().Equals("0")) return;
 				}
 			}
 		}
 
 		// OPTION 4 USER INPUT/LOOP
+		// ??
+		// Pre-condition: nil
+		// Post-condition: nil
 		private static void DeregisterMember()
 		{
 			while (true)
 			{
 				Header();
-				Console.WriteLine("Please enter member to delete information...");
+				Console.WriteLine("Please enter info for member to delete...");
 
 				// full names are unique, therefore do not worry about confirming phone
 				Console.Write("First name: ");
@@ -129,9 +260,7 @@ namespace CommunityLibrary
 				Console.Write("Last name: ");
 				string last = Console.ReadLine();
 
-				IMember m = new Member(first, last);
-
-				if (StaffFunctions.DeregisterMember(m))
+				if (StaffFunctions.DeregisterMember(first, last))
 				{
 					Console.Write($"\nMember {first} {last} removed...\nEnter any key to continue: ");
 					Console.ReadLine();
