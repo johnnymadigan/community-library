f32f3910849de38ec769d9a8c19a0fdf93789745
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2022-05-17 15:28:35 +1000

    Updated 2 functions to meet requirements
    Add/remove dvd now increments/decrements copies AND add if brand new, remove if 0 copies

--- CommunityLibrary/Menus/StaffFunctions.cs
+++ CommunityLibrary/Menus/StaffFunctions.cs
@@ -1,25 +1,68 @@
-﻿using System;
+﻿// STAFF FUNCTIONS
+// Functions used by the staff account (called only from StaffMenu)
+// Seperates display/user-input from key functionality
+// All methods are public for corresponding test plan (TestStaffFunctions)
+using System;
+
 namespace CommunityLibrary
 {
 	public class StaffFunctions
 	{
-		// OPTION 1 FUNCTIONALITY (seperate + public for testing)
+		// OPTION 1 ===========================================================
+		// Add new DVDs of a movie to the system (NEW MOVIE)
+		// Pre-condition: Movie is new (library does not contain this movie)
+		// Post-condition: Return true if movie is successfully inserted, false otherwise (prevents duplicates)
 		public static bool AddDVD(IMovie m)
 		{
-			
-			return Records.lib.Insert(m); // true if inserted into BST, false if dupe
+			return Records.lib.Insert(m);
 		}
 
-		// OPTION 2 FUNCTIONALITY (seperate + public for testing)
-		public static bool RemoveDVD(IMovie m)
+		// OPTION 1 ===========================================================
+		// Add new DVDs of a movie to the system (EXISTING MOVIE)
+		// Pre-condition: Movie is not new (library contains this movie)
+		// Post-condition: Increment and return total/available copies of the movie
+		public static int AddDVD(string title)
 		{
-			return Records.lib.Delete(m); // true if removed from BST, false if not found
+			IMovie movie = Records.lib.Search(title); ;
+
+			if (movie != null)
+			{
+				movie.TotalCopies++;
+				movie.AvailableCopies++;
+
+				return movie.TotalCopies;
+			}
+			else return -1;
+			
 		}
 
-		// OPTION 3 FUNCTIONALITY (seperate + public for testing)
+		// OPTION 2 ===========================================================
+		// Remove DVDs of a movie from the system
+		// Pre-condition: nil
+		// Post-condition: Decrement and return the total/available copies of the movie, otherwise -1 if movie does not exist
+		public static int RemoveDVD(string title)
+		{
+			IMovie movie = Records.lib.Search(title); ;
+
+			if (movie != null)
+            {
+				movie.TotalCopies--;
+				movie.AvailableCopies--;
+
+				if (movie.TotalCopies == 0) Records.lib.Delete(movie); // delete from library if no more copies
+
+				return movie.TotalCopies--;
+			}
+			else return -1;
+		}
+
+		// OPTION 3 ===========================================================
+		// Register a new member with the system
+		// Pre-condition: nil
+		// Post-condition: Return true if member is added (registered), false if dupe or contact/pin invalid
 		public static bool RegisterMember(IMember m)
 		{
-			foreach (IMember member in Records.reg) if (member.CompareTo(m) == 0) return false; // return false if dupe
+			foreach (IMember member in Records.reg) if (member.CompareTo(m) == 0) return false; // false if dupe
 
 			if (!IMember.IsValidContactNumber(m.ContactNumber) || !IMember.IsValidPin(m.Pin)) return false; // false if contact/pin invalid
 			else
@@ -29,7 +72,10 @@ namespace CommunityLibrary
 			}
 		}
 
-		// OPTION 4 FUNCTIONALITY (seperate + public for testing)
+		// OPTION 4 ===========================================================
+		// Remove a registered member from the system
+		// Pre-condition: nil
+		// Post-condition: Return true if member is removed, false if not registered yet
 		public static bool DeregisterMember(IMember m)
 		{
 			foreach (IMember member in Records.reg)
@@ -42,6 +88,10 @@ namespace CommunityLibrary
 			}
 			return false;
 		}
+
+		// OPTION 5 ===========================================================
+
+		// OPTION 6 ===========================================================
 	}
 }
 
