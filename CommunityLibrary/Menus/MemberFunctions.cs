6e929db2404c9ad41867f14838da738be159e9b8
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2022-05-18 18:48:14 +1000

    Borrowers can now return
    need to implement user selection for this function

--- CommunityLibrary/Menus/MemberFunctions.cs
+++ CommunityLibrary/Menus/MemberFunctions.cs
@@ -20,15 +20,26 @@ namespace CommunityLibrary
 		// Post-condition: Return true if member was added to the movie's borrowing list, false otherwise
 		public static bool BorrowDVD(IMovie movie, IMember m)
 		{
-			// If member is not at borrowing limit (5 movies), and movie exists in library...
+			// If member is not at borrowing limit (5 movies), and movie exists in records...
 			// attempt to add (fails if already borrowing, at max borrowers (10), or no available copies)
-			if (Records.GetMemberBorrowings(m).Count <= 5 && Records.lib.Search(movie))
+			if (Records.GetMemberBorrowings(m).Count < 5 && Records.lib.Search(movie))
 				return Records.lib.Search(movie.Title).AddBorrower(m);
 			else return false;
 		}
 
 		// OPTION 4 ===========================================================
-		/* todo */
+		// Allow a registered member to return a DVD
+		// Pre-condition: Movie and member exist in records, member currently in movie's borrowing collection
+		// Post-condition: Return true if member was borrowing and now no longer, false otherwise
+		public static bool ReturnDVD(IMovie movie, IMember m)
+		{
+			if (Records.lib.Search(movie))
+            {
+				// return true if removed, false otherwise
+				return Records.lib.Search(movie.Title).RemoveBorrower(m);
+			}
+			else return false;
+		}
 
 		// OPTION 5 ===========================================================
 		/* todo */
