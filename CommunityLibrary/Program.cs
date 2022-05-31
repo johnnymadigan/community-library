016b082964f781cd00d41b9b881b2ccd03cf6c11
johnnymadigan <74356539+johnnymadigan@users.noreply.github.com>
2022-05-31 18:28:00 +1000

    capture WORST-CASE only

--- CommunityLibrary/Program.cs
+++ CommunityLibrary/Program.cs
@@ -23,25 +23,37 @@ namespace CommunityLibrary
 
         // EMPIRCAL ANALYSIS on TOPTHREE ALGORITHM
         // Pre-condition: Nil
-        // Post-condition: Generates sample data for a series of tests and...
+        // Post-condition: Generates worst-case sample data for a series of tests and...
         //                 display the number of times basic operation is executed
         static void Emperical()
         {
             IMovieCollection lib;
             Random r = new Random();
 
-            for (int i = 1; i <= 10; i++) // 10 TESTS/INPUT SAMPLES (1000, 2000, ... 10,000)
+            // 10 TESTS/INPUT SAMPLES (1000, 2000, ... 10,000)
+            for (int i = 1; i <= 10; i++)
             {
                 lib = new MovieCollection();
 
-                for (int j = 0; j <= i * 1000; j++) // CREATE INPUT SAMPLES (fill library up with movies)
+                // CREATE WORST-CASE INPUT SAMPLES (fill library up with movies)
+                // As we are only interested in the WORST-CASE...
+                // set first and second place at the beginning, and the rest in ascending order...
+                // this means 3rd place always updates, forcing ALL basic operations to execute
+                IMovie first = new Movie($"always first");
+                IMovie second = new Movie($"always second");
+                first.NoBorrowings = 2000000; // 2 million
+                second.NoBorrowings = 1000000; // 1 million
+                lib.Insert(first);
+                lib.Insert(second);
+
+                for (int j = 3; j <= i * 1000; j++) // ascending order so 3rd place always updates
                 {
-                    IMovie temp = new Movie($"{j}", MovieGenre.Action, MovieClassification.G, 1, 100);
-                    temp.NoBorrowings = r.Next(101); // generate random NoBorrowings for each movie [0..100]
+                    IMovie temp = new Movie($"{j}");
+                    temp.NoBorrowings = j;
                     lib.Insert(temp);
                 }
 
-                // Run algo and display result
+                // DISPLAY RESULT
                 int count = MemberFunctions.TopThree(lib.ToArray());
                 Console.WriteLine($"Input: {i * 1000}\tCount: {count}\n");
             }
